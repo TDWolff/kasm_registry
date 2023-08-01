e5eb4420a4bf60cdf85225d0ebe4bd370af611f7
jm1021 <54686997+jm1021@users.noreply.github.com>
2023-08-01 15:18:57 -0700

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     name: 'Del Norte HS Computer Science',
     description: 'The official store for Kasm supported workspaces.',
     icon: '/img/logo.svg',
-    listUrl: 'https://nighthawkcoders.github.io/kasm_registry/1.0/',
+    listUrl: 'https://github.com/nighthawkcoders/kasm_registry/',
     contactUrl: 'https://github.com/nighthawkcoders/kasm_registry/issues',
   },
   reactStrictMode: true,
